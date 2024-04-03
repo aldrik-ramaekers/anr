1afc974d3681b9d98a1c76915910a3fa4306419c
Aldrik Ramaekers <aldrikboy@gmail.com>
2024-04-03 23:32:44 +0200

    good evening

--- anr_sc.h
+++ anr_sc.h
@@ -33,9 +33,49 @@ before you include this file in *one* C file to create the implementation.
 
 typedef enum
 {
-	ANR_SC_SIMPLE,
-	ANR_SC_RAW,
-} anr_sc_type;
+	ANR_SC_FLAG_SIMPLE,
+	ANR_SC_FLAG_RAW,
+} anr_sc_flag;
+
+typedef enum
+{
+	ANR_SC_BLOCK_RAW = 0,
+	ANR_SC_BLOCK_COMPRESSED = 1,
+	ANR_SC_BLOCK_REPEAT = 2,
+} anr_sc_block_type;
+
+/*
+
+file flag:
+byte 0: 0 = compressed, 1 = raw
+
+file blocks:
+raw block: 2 - 63 bytes.
+compressed block: 2 bytes.
+2 pair repeat block: 5 bytes.
+
+after byte 1:
+bit 0-1 = 0 = raw, 1 = compressed 2 = repeated
+bit 1-7 = count (max 63)
+
+if raw: copy count number of bytes.
+if compressed: repeat second byte count number of times.
+if repeated: byte 1,3 = repeat count, byte 2,4 = repeat data, count = how many times to repeat cycle.
+
+DEFLATE STEP 1: compress all bytes.
+DEFLATE STEP 2: find repeating blocks.
+*/
+
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  ((byte) & 0x80 ? '1' : '0'), \
+  ((byte) & 0x40 ? '1' : '0'), \
+  ((byte) & 0x20 ? '1' : '0'), \
+  ((byte) & 0x10 ? '1' : '0'), \
+  ((byte) & 0x08 ? '1' : '0'), \
+  ((byte) & 0x04 ? '1' : '0'), \
+  ((byte) & 0x02 ? '1' : '0'), \
+  ((byte) & 0x01 ? '1' : '0') 
 
 ANRSCDEF void* anr_sc_deflate(void* data, uint32_t length, uint32_t* out_length);
 
@@ -43,45 +83,144 @@ ANRSCDEF void* anr_sc_deflate(void* data, uint32_t length, uint32_t* out_length)
 
 #ifdef ANR_SC_IMPLEMENTATION
 
-void* anr_sc_deflate(void* data, uint32_t length, uint32_t* out_length)
-{
-	uint32_t max_size = length*2 + 1;
-	uint8_t* final_stream = malloc(max_size);
-	uint32_t write_cursor = 0;
-
-	final_stream[0] = ANR_SC_SIMPLE;
-	write_cursor++;
-	
+static uint32_t anr_sc_deflate_step_1(void* data, uint32_t length, uint8_t* buffer, uint32_t cursor)
+{	
 	uint8_t* src = (uint8_t*)data;
 	uint8_t cmp = 0;
 	uint8_t count = 0;
 	for (int i = 0; i < length; i++)
 	{
 		uint8_t val = src[i];
-		if (val == cmp) count++;
-		else {
-			if (count > 0) {
-				final_stream[write_cursor++] = count;
-				final_stream[write_cursor++] = cmp;
-			}
-			cmp = val;
-			count = 1;
+		if (val == cmp) {
+			count++;
+
+			if (count <= 63) // max number per block.
+				continue;
 		}
-	}
-	final_stream[write_cursor++] = count;
-	final_stream[write_cursor++] = cmp;
 
-	if (write_cursor > length) {
-		memcpy(final_stream+1, data, length);
-		write_cursor = length;
-		final_stream[0] = ANR_SC_RAW;
-
+		if (count > 0) {
+			if (count > 63) count = 63;
+			count |= (ANR_SC_BLOCK_COMPRESSED<<6);
+			buffer[cursor++] = count;
+			buffer[cursor++] = cmp;
+		}
+		cmp = val;
+		count = 1;
 	}
+	// Finalize last block.
+	count |= (ANR_SC_BLOCK_COMPRESSED<<6);
+	buffer[cursor++] = count;
+	buffer[cursor++] = cmp;
+
+	return cursor;
+}
+
+static uint32_t anr_sc_deflate_step_2(void* data, uint32_t length, uint8_t* buffer, uint32_t cursor)
+{
+	ANRSC_ASSERT(cursor % 2 == 1);
+
+	for (uint32_t i = 1; i < cursor; i+=2)
+	{
+		uint8_t count = buffer[i];
+		//uint8_t val = buffer[i+1];
+		uint8_t type = count>>6;
+		//printf("%d %d\n", count&0x3F, val);
+		ANRSC_ASSERT(type == ANR_SC_BLOCK_COMPRESSED);
+		count &= 0x3F;
+
+
+		uint8_t pair1_count = 0;
+		uint8_t pair1_val = 0;
+		uint8_t pair2_count = 0;
+		uint8_t pair2_val = 0;
+		uint8_t pair_count = 0; // need atleast 3 pairs for this to pay off.
+		uint8_t finding_pairs = 1;
+		for (int x = i; x < cursor; x+=4)
+		{
+			uint8_t p1_count = buffer[x]&0x3F;
+			uint8_t p1_val = buffer[x+1];
+			uint8_t p2_count = buffer[x+2]&0x3F;
+			uint8_t p2_val = buffer[x+3];
+			if (finding_pairs)
+			{
+				pair1_count = p1_count;
+				pair1_val = p1_val;
+				pair2_count = p2_count;
+				pair2_val = p2_val;
+				finding_pairs = 0;
+				pair_count = 1;
+				continue;
+			}
+
+			if (p1_count == pair1_count && p2_count == pair2_count && p1_val == pair1_val && p2_val == pair2_val && pair_count)
+			{
+				pair_count++;
+
+				if (pair_count <= 63) // max number per block.
+				{
+					if (x+1 < cursor)
+						continue;
+				}
+			}
+
+			if (pair_count >= 3) { // Less than 3 pairs is better off raw.
+				// Insert pair into current block.
+				uint32_t saved_space = pair_count*4;
+				buffer[i] = pair_count;
+				buffer[i] |= (ANR_SC_BLOCK_REPEAT<<6);
+				buffer[i+1] = pair1_count;
+				buffer[i+2] = pair1_val;
+				buffer[i+3] = pair2_count;
+				buffer[i+4] = pair2_val;
+
+				uint8_t* write_start = buffer+i+5;
+				uint32_t read_offset = i+saved_space;
+				memmove(write_start, buffer + read_offset, cursor - read_offset); // TODO dont do this..
+
+				cursor -= saved_space;
+				cursor += 5;
+
+				//printf("Found pair! count: %d, saved: %d [%d %d %d %d]\n", pair_count, saved_space, pair1_count, pair1_val, pair2_count, pair2_val);
+
+				i += 3;
+			}
+			break;
+		}
+		
+		//printf("%d %d\n", buffer[i-12], buffer[i-10]);
+
+		//printf("%d %d\n", count&0x3F, val);
+	}
+
+	return cursor;
+}
+
+void* anr_sc_deflate(void* data, uint32_t length, uint32_t* out_length)
+{
+	uint32_t max_size = length*3 + 1;
+	uint8_t* final_stream = malloc(max_size);
+	uint32_t write_cursor = 0;
+
+	final_stream[0] = ANR_SC_FLAG_SIMPLE;
+	write_cursor++;
+
+	write_cursor = anr_sc_deflate_step_1(data, length, final_stream, write_cursor);
+	printf("Step 1: Compress: %d -> %d (%.1f%%)", length, write_cursor, length/(float)write_cursor*100.0f);
+	write_cursor = anr_sc_deflate_step_2(data, length, final_stream, write_cursor);
+	printf(" Step 2: -> %d (%.1f%%)\n", write_cursor, length/(float)write_cursor*100.0f);
+
+	//printf("%d %d "BYTE_TO_BINARY_PATTERN"\n", count, cmp, BYTE_TO_BINARY(count));
+	//if (write_cursor > length) {
+	//	memcpy(final_stream+1, data, length);
+	//	write_cursor = length;
+	//	final_stream[0] = ANR_SC_RAW;
+	//
+	//}
 
 	*out_length = write_cursor;
 
 	#ifdef ANR_SC_DEBUG
-	printf("Compress: %d -> (%.1f%%)\n", length, length/(float)write_cursor*100.0f);
+
 	#endif
 
 	return final_stream;
--- examples/test_sc.c
+++ examples/test_sc.c
@@ -5,6 +5,11 @@
 #include <time.h>
 #include <stdlib.h>
 
+int big_matrix[] = {
+	0,1,5,7,4,2,1,4,5,0xf,1,0,0,0,1,1,4,6,0xa,1,2,4,4,4,4,4,3,2,1,5,5,5,5,5,6,6,6,7,8,8,8,9,1,2,3,4,4,4,3,3,2,2,9,9,9,8,7,
+	1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+};
+
 int* random_matrix_2d(int x, int y, int percentage) {
 	srand(time(NULL));
 	int* matrix = malloc(x * y * sizeof(int));
@@ -17,14 +22,39 @@ int* random_matrix_2d(int x, int y, int percentage) {
 	return matrix;
 }
 
-int main(int argc, char** argv)
+void test_file(char* str)
 {
-	for (int i = 0; i < 100; i++) {
+	FILE* file = fopen(str, "rb");
+	fseek(file, 0, SEEK_END);
+	size_t iso_size = ftell(file);
+	rewind(file);
+	unsigned char* iso_buffer = malloc(iso_size);
+	fread(iso_buffer, 1, iso_size, file);
+	fclose(file);
+
+	uint32_t out;
+	anr_sc_deflate(iso_buffer, iso_size, &out);
+	free(iso_buffer);
+}
+
+int main(int argc, char** argv)
+{ 
+	// test_file("res/CorePlus-current.iso");
+	// test_file("res/bible.txt");
+	test_file("res/cid2code.txt");
+
+	#if 0
+	uint32_t out;
+	anr_sc_deflate(big_matrix, sizeof(big_matrix), &out);
+	/*
+	for (int i = 0; i < 50; i++) {
 		int* random_2d_matrix = random_matrix_2d(20, 20, 100 - i);
 
 		uint32_t out;
 		anr_sc_deflate(random_2d_matrix, 20*20*sizeof(int), &out);
-	}
+	}*/
+	#endif
+
 
 	return 0;
 }
